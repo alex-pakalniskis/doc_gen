4ea7ef102ab0e1b9737c6e02478ad84fe7dc572f
Alex Pakalniskis 3 <alexpakalniskis@gmail.com>
2022-10-20 20:21:44 -0700

    start enum parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use serde_yaml;
 use std::string::String;
 use graphql_parser::schema::parse_schema;
+use graphql_parser::schema::Field;
+use graphql_parser::schema::EnumValue;
 use graphql_parser::schema::Definition::TypeDefinition;
 use graphql_parser::schema::TypeDefinition::{Scalar, Object, Interface, Union, Enum, InputObject};
 mod ipfs;
@@ -29,18 +31,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut names: Vec<String> = Vec::new();
     let mut descriptions: Vec<Option<String>> = Vec::new();
+    let mut fields: Vec<Vec<Field<String>>> = Vec::new();
+    let mut enum_values: Vec<Vec<EnumValue<String>>> = Vec::new();
+
 
     for def in ast.definitions {
+
+        let mut tmp_fields: Vec<Field<String>> = Vec::new();
+        let mut tmp_enum_values: Vec<EnumValue<String>> = Vec::new();
+
         match def {
             TypeDefinition(Scalar(s)) => println!("{}: {:?}", s.name, s.description),
             TypeDefinition(Object(o)) => {
                 names.push(o.name);
-                descriptions.push(o.description)
+                descriptions.push(o.description);
 
+                for field in o.fields {
+                    tmp_fields.push(field)
+                }
+                fields.push(tmp_fields);
             },
             TypeDefinition(Interface(i)) => println!("{}: {:?}", i.name, i.description),
             TypeDefinition(Union(u)) => println!("{}: {:?}", u.name, u.description),
-            TypeDefinition(Enum(e)) => descriptions.push(e.description),
+            TypeDefinition(Enum(e)) => {
+                names.push(e.name);
+                descriptions.push(e.description);
+
+                for value in e.values {
+                    tmp_enum_values.push(value)
+                }
+                enum_values.push(tmp_enum_values);
+            },
             TypeDefinition(InputObject(io)) => println!("{}: {:?}", io.name, io.description),
             _ => todo!(),
         }
@@ -48,6 +69,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("{:?}\n", names);
     println!("{:?}\n", descriptions);
+    println!("{:?}\n", fields);
+    println!("{:?}\n", enum_values);
+
 
     Ok(())
 }
